858a7a4fab200396ce270d028ad37e17252bee00
ArokiaRaju <136999511+ArokiaRaju@users.noreply.github.com>
2023-06-23 01:44:12 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -16,13 +16,13 @@ TEST_CASE("infers the breach according to limits 2") {
 }
 
 TEST_CASE("classifyTemperature breach according to limits 1") {
-  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 12) == TOO_LOW); //inferBreach(12, 0, 35)
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -1) == TOO_LOW); //inferBreach(-1, 0, 35)
 }
 
 TEST_CASE("classifyTemperature breach according to limits 2") {
-  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 30) == TOO_HIGH); //inferBreach(30, 0, 45)
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 50) == TOO_HIGH); //inferBreach(50, 0, 45)
 }
 
 TEST_CASE("classifyTemperature breach according to limits 3") {
-  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 30) == NORMAL); //inferBreach(15, 0, 40)
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 30) == NORMAL); //inferBreach(30, 0, 40)
 }
