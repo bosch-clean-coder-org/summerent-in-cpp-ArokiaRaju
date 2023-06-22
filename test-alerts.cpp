02db974c4c6b3e08bca1e5d6b84d1b028e70d0c9
ArokiaRaju <136999511+ArokiaRaju@users.noreply.github.com>
2023-06-23 01:24:12 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -6,3 +6,11 @@
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
 }
+
+TEST_CASE("infers the breach according to limits 1111") {
+  REQUIRE(inferBreach(30, 20, 10) == TOO_HIGH);
+}
+
+TEST_CASE("infers the breach according to limits 2222") {
+  REQUIRE(inferBreach(15, 12, 32) == NORMAL);
+}
